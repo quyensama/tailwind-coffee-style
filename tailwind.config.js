270b9a73f5bf3035647d5ba45988147885b50d94
quyensama <nguyenquyen12345.2k@gmail.com>
2022-11-11 16:37:57 +0700

    add featured mugs - product card

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,15 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0%)' },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
         }
       },
       animation: {
         slideDown: 'slideDown 0.4s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out',
       },
       backgroundImage: {
         'slide-bg': 'url(".img/slider-bg.jpeg")'
